92892b10db2c204e47592fca209271f6e69a0fe5
Alexander Bondarenco <sasha.199568@gmail.com>
2025-07-13 19:22:47 +0300

    Vector

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,5 +5,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Scenes");
+
+        var a = new Vector(1, 2, 3);
+        System.out.println(a);
     }
 }
---
+++ app/src/main/java/org/example/Vector.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Vector {
+
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
